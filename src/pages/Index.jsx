4464a78aee72c1e3b60726c0fce2fe0d4f75e4c0
Mårten Wiman <marten.wiman@gmail.com>
2024-07-25 14:05:38 +0200

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -36,7 +36,7 @@ const Index = () => {
           <div className="flex space-x-2 mb-4">
             <Input
               type="text"
-              placeholder="Add a new todo"
+              placeholder="Add a NEW todo"
               value={newTodo}
               onChange={(e) => setNewTodo(e.target.value)}
               onKeyPress={(e) => e.key === 'Enter' && addTodo()}
